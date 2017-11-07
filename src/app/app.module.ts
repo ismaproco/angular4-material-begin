60a1a12cb898092c977c872ddd342413d98b06e3
Ismael <ismaproco@gmail.com>
2017-11-07 23:59:22 +0100

    Removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
-
 import { AppComponent } from './app.component';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({

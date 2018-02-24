01a5acc087f234cba54f5f6148b45db88efad1e7
Ismael Jimenez <ismaproco@gmail.com>
2018-02-24 17:56:23 +0100

    Updated to angular cli 1.7.0 and material 5.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
